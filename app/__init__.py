6284473f77dbc352a588c42b78c84fa5b93b06fb
zilonghe <495309675@qq.com>
2016-10-20 01:33:06 +0800

    sqlite set up done, and focusing on login part.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+from app import views, models
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
